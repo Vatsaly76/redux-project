2c2884fdd86c3f169ee2e46fdf23c78f7d5da9c8
Vatsaly76 <shuklavatsaly@gmail.com>
2026-01-17 01:06:55 +0530

    Add React Router and implement photo fetching feature
    - Updated .gitignore to include .env files
    - Added react-router-dom dependency in package.json and package-lock.json
    - Created mediaApi.js for fetching photos from Unsplash API
    - Implemented a button in App.jsx to fetch and display photos
    - Added new components: ResultCard.jsx, ResultGrid.jsx, SearchBar.jsx, Tabs.jsx, CollectionPage.jsx, collectionSlice.js, searchSlice.js, and store.js
    - Removed unused react.svg asset

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { fetchPhotos } from './api/mediaApi'
 
 const App = () => {
   return (
     <div className="h-screen w-full bg-[#222]">
       <h1 className="text-4xl font-bold text-white bg-amber-300">Redux Project</h1>
+      <button className='px-2 bg-amber-600 border-amber-50' 
+        onClick={async()=>{
+         const data = await fetchPhotos('nature')
+         console.log(data);
+      }}>Show Photos</button>
     </div>
   )
 }
