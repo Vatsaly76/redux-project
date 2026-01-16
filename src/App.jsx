7c1c38ae05e4b65c9b88ef248d418ffa902038e4
Vatsaly76 <shuklavatsaly@gmail.com>
2026-01-16 20:15:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className="h-screen w-full bg-[#222]">
+      <h1 className="text-4xl font-bold text-white bg-amber-300">Redux Project</h1>
+    </div>
+  )
+}
+
+export default App
