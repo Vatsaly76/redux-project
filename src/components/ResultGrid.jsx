f98a64fb3d229ef77db4264792a43857c6ce302c
Vatsaly76 <shuklavatsaly@gmail.com>
2026-01-20 21:57:42 +0530

    Refactor ResultGrid to improve data fetching logic and enhance error handling

--- src/components/ResultGrid.jsx
+++ src/components/ResultGrid.jsx
@@ -1,43 +1,62 @@
 import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { fetchPhotos, fetchVideos } from '../api/mediaApi'
-import { setQuery, setLoading, setError, setResults } from '../redux/features/searchSlice'
+import { setLoading, setError, setResults } from '../redux/features/searchSlice'
 import { useEffect } from 'react';
 
 const ResultGrid = () => {
+
+  const dispatch = useDispatch();
   const { query, activeTab, loading, error, results } = useSelector((store) => store.search);
 
-useEffect(function () {
-  const getData = async () => {
-    let data;
-    if (activeTab === 'photos') {
-      let response = await fetchPhotos(query);
-      data = response.results.map((item)=>({
-        id: item.id,
-        type: 'photo',
-        title: item.alt_description,
-        url: item.urls.small,
-        photographer: item.user.name
-      }));
+  useEffect(function () {
+    const getData = async () => {
+      try {
+        dispatch(setLoading(true));
+        let data = [];
+        if (activeTab === 'photos') {
+          let response = await fetchPhotos(query);
+          data = response.results.map((item) => ({
+            id: item.id,
+            type: 'photo',
+            title: item.alt_description,
+            url: item.urls.small,
+            photographer: item.user.name
+          }));
+        }
+        if (activeTab === 'videos') {
+          let response = await fetchVideos(query);
+          data = response.results.map((item) => ({
+            id: item.id,
+            type: 'video',
+            title: item.video_files[0].quality,
+            url: item.video_files[0].link,
+            photographer: item.user.name
+          }));
+        }
+        dispatch(setResults(data));
+      } catch (error) {
+        dispatch(setError(error.message));
+      } finally {
+        dispatch(setLoading(false));
+      }
     }
-    if (activeTab === 'videos') {
-      let response = await fetchVideos(query);
-      data = response.results.map((item) => ({
-        id: item.id,
-        type: 'video',
-        title: item.video_files[0].quality,
-        url: item.video_files[0].link,
-        photographer: item.user.name
-      }));
-    }
-    console.log(data);
-  }
-  getData()
-}, [query, activeTab]);
-return (
-  <div>
+    getData()
+  }, [query, activeTab, dispatch]);
 
-  </div>
-)
+  if (error) {
+    return <div className="text-center text-red-500">{error}</div>
+  }
+  if (loading) {
+    return <div className="text-center">Loading...</div>
+  }
+  return (
+    <div>
+      {results.map((item) => {
+        return item.title
+      })}
+    </div>
+  )
 }
 
 export default ResultGrid
